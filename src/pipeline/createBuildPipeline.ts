0f77066144437d0809b3b656a79d80c2d5bebfde
Ryan Kubik <rmkubik@me.com>
2026-08-02 15:22:08 -0700

    Disable debug

--- src/pipeline/createBuildPipeline.ts
+++ src/pipeline/createBuildPipeline.ts
@@ -74,6 +74,6 @@ export function createBuildPipeline(
       )
       .emit(new WriteHtmlContentFiles(outputDirectory))
       .emit(new EmitRssFeed("/blog/", outputDirectory))
-      .enableDebug()
+    // .enableDebug()
   );
 }
